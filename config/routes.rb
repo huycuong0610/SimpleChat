dd79fdaa2ee9a82765f70bc57cff99521086c3a5
huycuong0610 <huycuong0610@gmail.com>
2017-06-21 01:37:26 +0700

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'friendship/create'
+
+  get 'friendship/destroy'
+
+  resources :sessions, only: [:new, :create]
+  resources :users do
+  	resources :messages
+  end
+  delete '/logout' => 'sessions#destroy'
+  get '/message/show' =>'messages#show'
+  get '/show' => 'users#show'
+  get '/sent' => 'users#sent'
+  get '/alluser'	=> 'users#all_user'
+  root "users#index"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
